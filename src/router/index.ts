c1eeff88fb443525f32a76e005885c246d916dc5
LOONA\루나 <johs970917@gmail.com>
2025-11-06 00:59:38 +0900

    홈 화면 구상

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
+import HomeView from '@/views/HomeView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [{ path: '/', name: 'Home', component: HomeView }]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import BaseLayout from '@/layouts/BaseLayout.vue'
+</script>
+
+<template>
+  <BaseLayout>
+    <template #header>
+      <div>
+        <div class="flex justify-between">
+          <div class="flex">
+            <div>계정</div>
+            <div>재화</div>
+          </div>
+          <div>메인 네비</div>
+        </div>
+      </div>
+    </template>
+
+    <template #main>
+      <div>
+        <div class="flex justify-between">
+          <div>이벤트</div>
+          <div class="flex">
+            <div>캐릭터</div>
+            <div>캐릭터 정보</div>
+          </div>
+          <div>서브 네비</div>
+        </div>
+      </div>
+    </template>
+
+    <template #footer>
+      <div>
+        <div class="flex"></div>
+      </div>
+    </template>
+  </BaseLayout>
+</template>
+
+<style scoped></style>
